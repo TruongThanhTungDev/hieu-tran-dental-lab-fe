5a616fa1153b49f9aa98fe9cb49e5897e6b76f84
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2024-08-18 14:07:44 +0700

    update

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://103.75.184.157:8081',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://103.216.117.76:8080',
+  // baseUrl: 'http://localhost:3000',
 };
 
